ca6f3025002be19f41ff67e00d895612de137f57
Subramanian Murugan <sasubramanian_md@hotmail.com>
2022-04-14 12:15:30 +0530

    types static dynamic

---
+++ groovy-gettingstarted/src/TypeDemo.java
@@ -0,0 +1,24 @@
+public class TypeDemo {
+    public static void main(String[] args) {
+        byte a = 10;  //byte <=== int
+        short s = 100; // short <===int
+        int i = 10;   // int <=== int
+        long lng = 1000; //long <== int
+
+        //explicit conversion
+        byte b = (byte) 10000;
+
+
+        //here f is sufffix used to convert double to float
+        float f = 10.5f;  // default type of precision is double
+        float f1 = (float) 10.89;
+
+        //Reference Type
+        Integer myNumber =100;
+        //primitive type
+        int myNewNumber =myNumber; //int <== Integer -  Boxing
+
+        Integer myNumber2 = myNewNumber; //Integer <=== int Unboxing
+
+    }
+}
---
+++ groovy-gettingstarted/src/decimalNumbers.groovy
@@ -0,0 +1,5 @@
+//Decimal Numbers
+float price = 10.5f; // 4 bytes
+println("Float => " + price)
+double distance = 187.7865; //8 bytes
+println("Double => " + distance);
